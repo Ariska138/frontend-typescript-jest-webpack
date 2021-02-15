4a7624af05cbadbf916f786535e4887e9083d384
Ariska Hidayat <ariska138@gmail.com>
2021-02-15 09:52:39 +0700

    add jquery

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 //import application level css
 import './styles/main.scss'
 import {myTest} from 'app/test'
+import jquery = require("jquery");
+const $: JQueryStatic = jquery;
 
 //application bootstrap goes here
 document.getElementsByTagName('body')[0].innerText = myTest() ? "yup" : "nope";
