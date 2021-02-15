37424647a29c85448e14a5469d4a8c8206eedd6e
Ariska Hidayat <ariska138@gmail.com>
2021-02-15 09:44:41 +0700

    optimize webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -44,7 +44,29 @@ const config: webpack.Configuration = {
             errorDetails: true,
         },
         overlay: true
-    }
+    },
+    optimization: {
+        splitChunks: {
+          chunks: 'async',
+          minSize: 20000,
+          maxSize: 0,
+          minChunks: 1,
+          maxAsyncRequests: 30,
+          maxInitialRequests: 30,
+          cacheGroups: {
+            defaultVendors: {
+              test: /[\\/]node_modules[\\/]/,
+              priority: -10,
+              reuseExistingChunk: true
+            },
+            default: {
+              minChunks: 2,
+              priority: -20,
+              reuseExistingChunk: true
+            }
+          }
+        }
+      }
 }
 
 export default config;
